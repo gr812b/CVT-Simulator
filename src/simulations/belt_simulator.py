6a97402d13415ea33ba8a3639c47cc1fb14717f8
Kai Arseneau <kaiarseneau@hotmail.com>
2025-02-01 21:47:20 -0500

    Initial clamping + centrifugal math

---
+++ src/simulations/belt_simulator.py
@@ -0,0 +1,24 @@
+import numpy as np
+from utils.theoretical_models import TheoreticalModels as tm
+
+class BeltSimulator:
+  def __init__(self, density: float, cross_sectional_area: float):
+    self.density = density
+    self.cross_sectional_area = cross_sectional_area
+
+  def calculate_centrifugal_force(self, ω: float, shift_distance: float, sheave_angle: float, wrap_angle: float) -> float:
+    radius = tm.shift_to_pulley_radius_prim(shift_distance, sheave_angle)
+
+    length = radius * wrap_angle
+    mass = self.density * self.cross_sectional_area * length
+
+    return tm.centrifugal_force(mass, ω, radius)
+  
+  def radial_force_from_clamping(self, clamping_force: float, sheave_angle: float) -> float:
+    return 2 * clamping_force * np.tan(sheave_angle / 2)
+  
+  def calculate_net_radial_force(self, ω: float, shift_distance: float, sheave_angle: float, wrap_angle: float, clamping_force: float) -> float:
+    centrifugal_force = self.calculate_centrifugal_force(ω, shift_distance, sheave_angle, wrap_angle)
+    radial_force = self.radial_force_from_clamping(clamping_force, sheave_angle)
+    distribution_factor = 2 * np.sin(wrap_angle / 2)
+    return (centrifugal_force + radial_force) * distribution_factor
