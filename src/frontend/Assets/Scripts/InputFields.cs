60a6959ba23e795297633a2d01f77d7e7bf5b454
Travis Wing <98197814+t-wing11@users.noreply.github.com>
2025-02-04 16:39:02 -0500

    Added negative number constraint, added units (#160)

--- src/frontend/Assets/Scripts/InputFields.cs
+++ src/frontend/Assets/Scripts/InputFields.cs
@@ -73,7 +73,7 @@ public class InputFields : MonoBehaviour
     }
 
     private void validInput(string value, ref double field, ref TMP_InputField inputField, ref TMP_Text errorText) {
-        if (double.TryParse(value, out double val)) {
+        if (double.TryParse(value, out double val) && val >= 0) {
             field = val;
             inputField.image.color = Color.white;
             errorText.text = "";
