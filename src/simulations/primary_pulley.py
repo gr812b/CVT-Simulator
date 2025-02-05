182bdb650b2c319d5e8d152ca4a7ba553b813b4b
Kai Arseneau <83799062+gr812b@users.noreply.github.com>
2025-02-05 17:51:08 -0500

    Add ramp math (#165)
    * Initial clamping + centrifugal math
    
    * Added belt slip calculations
    
    * Formatted
    
    * Cleaned up forces
    
    * Radial forces
    
    * Used more constants and correct math
    
    * Added better constraints
    
    * Belt wrap angle
    
    * Formatting
    
    * Formatting and cleanup
    
    * Updated regeneration of data
    
    * Added ramps
    
    * Good enough
    
    * Added ramps
    
    * Formatting

--- src/simulations/primary_pulley.py
+++ src/simulations/primary_pulley.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils.theoretical_models import TheoreticalModels as tm
+from utils.ramp_representation import LinearSegment, CircularSegment, PiecewiseRamp
 
 
 class PrimaryPulley:
@@ -15,6 +16,12 @@ class PrimaryPulley:
         self.initial_compression = initial_compression
         self.flyweight_mass = flyweight_mass
         self.initial_flyweight_radius = initial_flyweight_radius
+        # Initializing ramp
+        self.ramp = PiecewiseRamp()
+        self.ramp.add_segment(LinearSegment(x_start=0, x_end=0.01, slope=-0.75))
+        self.ramp.add_segment(
+            CircularSegment(x_start=0.01, x_end=0.05, radius=0.25, theta_fraction=0.95)
+        )
 
     def calculate_flyweight_force(
         self, shift_distance: float, angular_velocity: float
@@ -24,7 +31,12 @@ class PrimaryPulley:
             angular_velocity,
             self.initial_flyweight_radius,  # TODO: Add some distance here
         )
-        return centrifugal_force * np.tan(np.pi / 6)  # TODO: Calculate ramp angle
+        if shift_distance < 0:
+            shift_distance = 0
+        if shift_distance > 0.05:
+            shift_distance = 0.05
+        angle = np.arctan(np.abs(self.ramp.slope(shift_distance)))
+        return centrifugal_force * np.sin(angle)  # TODO: Calculate ramp angle
 
     def calculate_spring_comp_force(self, compression: float) -> float:
         return tm.hookes_law_comp(
