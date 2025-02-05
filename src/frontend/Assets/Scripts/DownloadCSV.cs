971d4540bcb65e71a48675250366733769d8ecef
Travis Wing <98197814+t-wing11@users.noreply.github.com>
2025-02-05 15:42:37 -0500

    Download Data  (#141)
    * Added download button functionality
    
    * Removed sample code
    
    * put script back in

---
+++ src/frontend/Assets/Scripts/DownloadCSV.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using SFB; 
+
+public class DownloadCSV : MonoBehaviour
+{
+    private readonly string csvPath = Path.Combine(Application.dataPath, "../simulation_output.csv");
+
+    [SerializeField] private Button downloadButton;
+
+    private void Start() {
+        downloadButton.onClick.AddListener(Download);
+    }
+
+    private void Download() {
+       
+        string destinationPath = StandaloneFileBrowser.SaveFilePanel(
+            "Save CSV File",        // Title of the dialog
+            "",                    // Initial directory (empty defaults to user's Documents)
+            "simulation_output",   // Default file name
+            "csv"                  // Default file extension
+        );
+
+
+        if (!string.IsNullOrEmpty(destinationPath)) {
+            File.Copy(csvPath, destinationPath, true);
+            Debug.Log("Downloaded CSV file to: " + destinationPath);
+        } else {
+            Debug.Log("Save operation canceled.");
+        }
+    } 
+}
