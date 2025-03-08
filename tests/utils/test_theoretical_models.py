937a000b374c403ce37a39abdd790473b6002b76
t-wing11 <Traviswing20@gmail.com>
2025-03-08 16:30:15 -0500

    removed comments

--- tests/utils/test_theoretical_models.py
+++ tests/utils/test_theoretical_models.py
@@ -33,22 +33,22 @@ class TestTheoreticalModels(unittest.TestCase):
         self.assertAlmostEqual(TheoreticalModels.newtons_second_law(10, 9.8), 98)
 
     def test_outer_prim_radius(self):
-        self.assertAlmostEqual(TheoreticalModels.outer_prim_radius(-3.955310010320904e-06), 0.0346202)  # Replace with actual expected value
+        self.assertAlmostEqual(TheoreticalModels.outer_prim_radius(-3.955310010320904e-06), 0.0346202) 
 
     def test_outer_sec_radius(self):
-        self.assertAlmostEqual(TheoreticalModels.outer_sec_radius(-3.955255), 0.1015999999)  # Replace with actual expected value
+        self.assertAlmostEqual(TheoreticalModels.outer_sec_radius(-3.955255), 0.1015999999)  
 
     def test_current_cvt_ratio(self):
-        self.assertAlmostEqual(TheoreticalModels.current_cvt_ratio(-3.955255), 3.4959772834831964)  # Replace with actual expected value
+        self.assertAlmostEqual(TheoreticalModels.current_cvt_ratio(-3.955255), 3.4959772834831964) 
 
     def test_wrap_angle(self):
         self.assertAlmostEqual(TheoreticalModels.wrap_angle(0.026835099999999997, 0.09381489999999991), 0.2646595431372414)
 
     def test_primary_wrap_angle(self):
-        self.assertAlmostEqual(TheoreticalModels.primary_wrap_angle(-3.955289409632892e-06), 2.8769331104525517)  # Replace with actual expected value
+        self.assertAlmostEqual(TheoreticalModels.primary_wrap_angle(-3.955289409632892e-06), 2.8769331104525517) 
 
     def test_secondary_wrap_angle(self):
-        self.assertAlmostEqual(TheoreticalModels.secondary_wrap_angle(-3.955289409632892e-06), 3.4062521967270345)  # Replace with actual expected value
+        self.assertAlmostEqual(TheoreticalModels.secondary_wrap_angle(-3.955289409632892e-06), 3.4062521967270345) 
 
 if __name__ == '__main__':
     unittest.main()
