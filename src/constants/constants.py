7609f84081b51a04069d575bd549c035c4d462aa
Kai Arseneau <kaiarseneau@hotmail.com>
2025-03-13 20:10:37 -0400

    Renamed variable

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -3,5 +3,5 @@ from scipy.constants import g
 AIR_DENSITY = 1.225  # kg/m^3
 GRAVITY = g  # m/s^2
 RUBBER_DENSITY = 1100  # kg/m^3
-RUBBET_ALUMINUM_STATIC_FRICTION = 0.8  # unitless
+RUBBER_ALUMINUM_STATIC_FRICTION = 0.8  # unitless
 RUBBER_ALUMINUM_KINETIC_FRICTION = 0.65  # unitless
