b631edff9ce20d5125abfa0b4da84faac784607b
Cameron Dunn <67480325+camdnnn@users.noreply.github.com>
2025-03-20 01:55:33 -0400

    show progress using terminal (#218)

--- src/frontend/Assets/Scripts/Constants.cs
+++ src/frontend/Assets/Scripts/Constants.cs
@@ -11,8 +11,6 @@ public static class PathConstants
 
     public static string SIMULATION_OUTPUT_PATH = Relativize("../simulation_output.csv");
     public static string INPUT_PARAMETERS_PATH = Relativize("../input_parameters.csv");
-    public static string PERCENT_VALUE_PATH = Relativize("../progress_percent.csv");
-    public static string PROGRESS_LOCKFILE_PATH = Relativize("../progress.lock");
     public static string PYTHON_ENVIRONMENT_PATH = Relativize("../../../venv/Scripts/python.exe");
     public static string PYTHON_SCRIPT_PATH = Relativize("../../main.py");
     public static string GRAPH_SCRIPT_PATH = Relativize("../../utils/generate_graphs.py");
