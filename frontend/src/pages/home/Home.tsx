640f5b49b6661edf6f59f7a8f62dff27c53d0beb
camdnnn <cameron634@gmail.com>
2025-05-20 02:12:08 -0400

    renamed app to home

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,10 +1,10 @@
 import styles from './Home.module.scss'
 import bajaLogo from '@assets/baja_logo.png'
 
-function App() {
+function Home() {
 
   return (
-    <div className={styles.App}>
+    <div className={styles.Home}>
       <div className={styles.header}>
         <img className={styles.logo} src={bajaLogo} alt="Baja Logo" />
         <h1 className={styles.title}>CVT Simulator</h1>
@@ -13,4 +13,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
