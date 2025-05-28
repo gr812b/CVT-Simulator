f8c07d4a40548d2f58bc58eb1c9271561f7af7ea
Cameron Dunn <67480325+camdnnn@users.noreply.github.com>
2025-05-28 12:56:01 -0400

    Main Button Component (#261)
    * added button
    
    * implemented icon button
    
    * added button class
    
    * added base styling
    
    * updated icon props
    
    * Replaced FC and SVGProps with ComponentType and SVGAttributes
    
    * made invertButton
    
    * changed to main button
    
    * made text optional

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import '@styles/_base.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -7,7 +8,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import { resolve } from 'path'
 
 const root = resolve(__dirname, 'src');
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@assets': resolve(root, 'assets'),
