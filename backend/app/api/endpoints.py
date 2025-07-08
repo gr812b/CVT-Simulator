faa406569a576fe55c1a0c88eed7e9304a8e5108
Kai Arseneau <83799062+gr812b@users.noreply.github.com>
2025-07-08 15:28:02 +0000

    Added backend

---
+++ backend/app/api/endpoints.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Body
+
+router = APIRouter()
+
+# curl -X POST "http://127.0.0.1:8000/compute" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"field\":\"test input\"}"
+@router.post("/compute")
+def compute(field: str = Body(..., embed=True)):
+    result = f"Received: {field}"
+    return {"result": result}
+
+@router.get('/')
+def ping():
+    return "pong"
