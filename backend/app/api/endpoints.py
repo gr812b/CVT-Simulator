b48d301a0062d0cbb1938c0cb061a0de85652e6e
Kai Arseneau <83799062+gr812b@users.noreply.github.com>
2025-07-08 15:49:06 +0000

    Linting

--- backend/app/api/endpoints.py
+++ backend/app/api/endpoints.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter, Body
 
 router = APIRouter()
 
+
 # curl -X POST "http://127.0.0.1:8000/compute" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"field\":\"test input\"}"
 @router.post("/compute")
 def compute(field: str = Body(..., embed=True)):
     result = f"Received: {field}"
     return {"result": result}
 
-@router.get('/')
+
+@router.get("/")
 def ping():
     return "pong"
