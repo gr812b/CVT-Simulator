2d08193e1d8a759e351e6f42d2f2a1527d1cf085
Cameron Dunn <67480325+camdnnn@users.noreply.github.com>
2025-09-18 13:45:19 -0600

    Landing Page (#295)
    * rename mainButton to button
    
    * renamed home to dashboard
    
    * added dashboard styling
    
    * updated svgs
    
    * add disabled styling

--- frontend/src/pages/app/App.tsx
+++ frontend/src/pages/app/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from '@pages/home/Home';
+import { Dashboard } from '@pages/dashboard/Dashboard';
 import { Input } from '@pages/input/Input';
 import { Playback } from '@pages/playback/Playback';
 
 export const App = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/' element={<Dashboard/>} />
             <Route path='/input' element={<Input/>} />
             <Route path='/playback' element={<Playback/>} />
             <Route path='*' element={<div>404 - Not found</div>} />
---
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,56 @@
+import styles from './Dashboard.module.scss'
+import bajaLogo from '@assets/baja_logo.png'
+import { Button } from '@components/button/Button';
+import { useNavigate } from 'react-router-dom';
+import Play from '@assets/icons/play.svg?react';
+import Edit from '@assets/icons/edit.svg?react';
+import TrashCan from '@assets/icons/trash_can.svg?react';
+import Plus from '@assets/icons/plus.svg?react';
+
+
+export const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const tempIsDisabled = true; // Placeholder for future functionality
+  
+  return (
+    <div className={styles.dashboard}>
+      <div className={styles.header}>
+        <img className={styles.logo} src={bajaLogo} alt="Baja Logo" />
+        <h1 className={styles.title}>CVT Simulator</h1>
+      </div>
+      <div className={styles.tableContainer}>
+        <span>No Saved Simulations...</span>
+      </div>
+      <div className={styles.buttonsContainer}>
+        <Button
+          text={'Run'}
+          icon={Play}
+          className={styles.button}
+          disabled={tempIsDisabled}
+          onClick={() => console.log('Run button clicked')}
+        />
+        <Button
+          text={'Edit'}
+          icon={Edit}
+          className={styles.button}
+          disabled={tempIsDisabled}
+          onClick={() => console.log('Edit button clicked')}
+        />
+        <Button
+          text={'Delete'}
+          icon={TrashCan}
+          className={styles.button}
+          disabled={tempIsDisabled}
+          onClick={() => console.log('Delete button clicked')}
+        />
+        <Button
+          text={'New'}
+          icon={Plus}
+          className={styles.button}
+          onClick={() => navigate('/input')}
+        />
+      </div>
+    </div>
+  )
+}
