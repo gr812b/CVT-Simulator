b7d6ec7629ddd6089891b47487224a7c256e8b5b
camdnnn <cameron634@gmail.com>
2025-09-18 11:50:53 -0600

    renamed home to dashboard

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,9 @@
-import styles from './Home.module.scss'
+import styles from './Dashboard.module.scss'
 import bajaLogo from '@assets/baja_logo.png'
 
-export const Home = () => {
+export const Dashboard = () => {
   return (
-    <div className={styles.Home}>
+    <div className={styles.dashboard}>
       <div className={styles.header}>
         <img className={styles.logo} src={bajaLogo} alt="Baja Logo" />
         <h1 className={styles.title}>CVT Simulator</h1>
