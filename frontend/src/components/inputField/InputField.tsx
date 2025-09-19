5dd65808fc602eb482540a8f4871ae1b5a571672
camdnnn <cameron634@gmail.com>
2025-09-19 09:17:30 -0600

    fixed styling

--- frontend/src/components/inputField/InputField.tsx
+++ frontend/src/components/inputField/InputField.tsx
@@ -12,8 +12,8 @@ interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 export const InputField = ({ label, error, className, ...props }: InputFieldProps) => {
   return (
     <div className={cx(styles.inputContainer, { [styles.hasError]: error }, className)}>
-      {label && <label className={cx(styles.label, { [styles.hasError]: error })}>{label}</label>}
-      <input {...props} className={cx(styles.input, { [styles.hasError]: error })} />
+      {label && <label className={styles.label}>{label}</label>}
+      <input {...props} className={styles.input} />
       <span className={styles.error}>{error ?? ''}</span>
     </div>
   );
