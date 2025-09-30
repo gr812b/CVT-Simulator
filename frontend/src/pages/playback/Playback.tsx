c399dd89525ff0b0121b0bfc4574c3118f01f739
camdnnn <cameron634@gmail.com>
2025-09-30 15:32:28 -0600

    added styling

--- frontend/src/pages/playback/Playback.tsx
+++ frontend/src/pages/playback/Playback.tsx
@@ -1,15 +1,35 @@
-import { Page } from '@components/graph2D/page';
 import { runSimulation } from '@utils/api';
+import { useNavigate } from 'react-router-dom';
+import styles from './Playback.module.scss';
+import { Button } from '@components/button/Button';
+import Home from '@assets/icons/home.svg?react';
+import Edit from '@assets/icons/edit.svg?react';
 
 export const Playback = () => {
+    const navigate = useNavigate();
 
     runSimulation().then((data) => {
         console.log(data);
     });
 
     return (
-        <div>
-            <Page />
+        <div className={styles.playback}>
+            <div className={styles.buttonsContainer}>
+            <Button
+                text={'Home'}
+                icon={Home}
+                className={styles.navigateButton}
+                onClick={() => navigate('/')}
+            />
+            <Button
+                text={'Edit'}
+                icon={Edit}
+                className={styles.navigateButton}
+                onClick={() => navigate('/input')}
+            />
+            </div>
+            <div className={styles.displayGrid}></div>
+            <div className={styles.playbarContainer}></div>
         </div>
     )
 }
