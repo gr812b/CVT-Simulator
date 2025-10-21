0736b6b67d5f574fc318e852370e3207e6d91005
Kai Arseneau <83799062+gr812b@users.noreply.github.com>
2025-10-21 17:36:13 +0000

    Test fix

--- cvtModel/test/utils/test_system_state.py
+++ cvtModel/test/utils/test_system_state.py
@@ -23,17 +23,19 @@ class TestSystemState(unittest.TestCase):
             car_position=10.0,
             shift_velocity=5.0,
             shift_distance=2.0,
+            engine_angular_velocity=100.0,
         )
-        expected_array = [30.0, 10.0, 5.0, 2.0]
+        expected_array = [30.0, 10.0, 5.0, 2.0, 100.0]
         self.assertEqual(state.to_array(), expected_array)
 
     def test_from_array(self):
-        array = [30.0, 10.0, 5.0, 2.0]
+        array = [30.0, 10.0, 5.0, 2.0, 100.0]
         state = SystemState.from_array(array)
         self.assertEqual(state.car_velocity, 30.0)
         self.assertEqual(state.car_position, 10.0)
         self.assertEqual(state.shift_velocity, 5.0)
         self.assertEqual(state.shift_distance, 2.0)
+        self.assertEqual(state.engine_angular_velocity, 100.0)
 
 
 if __name__ == "__main__":
