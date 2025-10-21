c5decb6b343c4ad269bd5ea8f099772b2d923a3f
Kai Arseneau <83799062+gr812b@users.noreply.github.com>
2025-10-21 15:30:24 +0000

    Removed comment

--- cvtModel/src/cvt_simulator/main.py
+++ cvtModel/src/cvt_simulator/main.py
@@ -15,12 +15,9 @@ def simulate_cvt_model(args: SimulationArgs, out_csv: str = "simulation_output.c
 
     return formatted
 
-
 def main():
     formatted = simulate_cvt_model(SimulationArgs())
     formatted.write_formatted_csv("front_end_output.csv")
-    # Plot engine_angular_velocity
-
 
 if __name__ == "__main__":
     main()
