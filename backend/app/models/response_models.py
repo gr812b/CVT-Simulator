7300300beda6e238da691c754e66798927417f55
Kai Arseneau <83799062+gr812b@users.noreply.github.com>
2025-12-18 20:00:46 -0500

    Adding custom ramps in API call

--- backend/app/models/response_models.py
+++ backend/app/models/response_models.py
@@ -1,6 +1,8 @@
 # backend/models/response_models.py
 from .auto_model import model_from_class, partial_model_from_class
-from cvt_simulator import FormattedSimulationResult, SimulationArgs
+from cvt_simulator import FormattedSimulationResult, SimulationArgs, PiecewiseRampConfig
 
+# Auto-generate Pydantic models
 SimulationArgsInput = partial_model_from_class(SimulationArgs)
 FormattedResultModel = model_from_class(FormattedSimulationResult)
+PiecewiseRampConfigModel = model_from_class(PiecewiseRampConfig)
