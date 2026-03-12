d0377e12702ad3c5f6c57c2c6005d2fe238575e3
Cameron Dunn <cameron634@gmail.com>
2026-03-11 18:13:32 -0600

    fixed requested changes

--- frontend/src/pages/input/Input.tsx
+++ frontend/src/pages/input/Input.tsx
@@ -18,7 +18,7 @@ import { mapParametersToApiBody } from '@utils/parameterMapping';
 import Home from '@assets/icons/home.svg?react';
 import ArrowUpCircle from '@assets/icons/arrow_up_circle.svg?react';
 import ArrowDownCircle from '@assets/icons/arrow_down_circle.svg?react';
-import Run from '@assets/icons/run.svg?react';
+import PlayOutline from '@assets/icons/play_outline.svg?react';
 import Edit from '@assets/icons/edit.svg?react';
 import styles from './Input.module.scss';
 
@@ -198,7 +198,7 @@ export const Input = () => {
                     />
                     <Button
                         text='Run'
-                        icon={Run}
+                        icon={PlayOutline}
                         onClick={handleSubmit}
                         disabled={!formState.isValid()}
                     />
