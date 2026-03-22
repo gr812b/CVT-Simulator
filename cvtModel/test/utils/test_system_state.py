f7306a5a463a14092cd668f2bb40bab1afb1b343
Kai Arseneau <kaiarseneau@hotmail.com>
2026-03-22 14:13:48 -0400

    Test fix

--- cvtModel/test/utils/test_system_state.py
+++ cvtModel/test/utils/test_system_state.py
@@ -1,6 +1,7 @@
 import unittest
 
 from cvt_simulator.utils.system_state import SystemState
+from cvt_simulator.constants.car_specs import MAX_SHIFT
 
 
 class TestSystemState(unittest.TestCase):
@@ -30,7 +31,8 @@ class TestSystemState(unittest.TestCase):
     def test_from_array(self):
         array = [2.0, 5.0, 100.0, 30.0]
         state = SystemState.from_array(array)
-        self.assertEqual(state.shift_distance, 2.0)
+        # Shift distance is clamped when fetching it
+        self.assertEqual(state.shift_distance, MAX_SHIFT)
         self.assertEqual(state.shift_velocity, 5.0)
         self.assertEqual(state.primary_pulley_angular_velocity, 100.0)
         self.assertEqual(state.secondary_pulley_angular_velocity, 30.0)
