096328e28e312b431461c8d40ddd264dbbbfd8d8
Kai Arseneau <83799062+gr812b@users.noreply.github.com>
2026-09-07 19:35:54 -0400

    Updated contracts

--- frontend/src/api/generated/backend.ts
+++ frontend/src/api/generated/backend.ts
@@ -1618,6 +1618,10 @@ export interface components {
         };
         /** ValidationError */
         ValidationError: {
+            /** Context */
+            ctx?: Record<string, never>;
+            /** Input */
+            input?: unknown;
             /** Location */
             loc: (string | number)[];
             /** Message */
